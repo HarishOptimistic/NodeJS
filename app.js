861ee427b6d06096eb8e2d77b5be35339347d060
Harish Radhakrishnan <harish@haplabz.com>
2020-06-10 20:27:34 +0530

    Express - Routes

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-const http = require("http");
-
 const express = require('express');
 
 const app = express();
 
-const server = http.createServer(app);
+app.use('/', (req, res, next) => {
+ console.log('This always runs!');
+ next();
+})
 
-server.listen(3001);
+//app.use used to add a new middleware function
+app.use('/add-product',(req, res, next) => {
+ console.log('In another middleware!')
+ res.send('<h1>The "Add Product" Page</h1>');
+});
+
+app.use('/',(req, res, next) => {
+ console.log('In another middleware!')
+ res.send('<h1>Hello from Express</h1>');
+});
+
+// const server = http.createServer(app);
+// server.listen(3000);
+app.listen(3000);
